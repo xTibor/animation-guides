5cae9864511a449e4d19a255ea71b067b9cffaf8
Nagy Tibor <xnagytibor@protonmail.com>
2023-03-06 17:17:29 +0100

    Add radial easing rulers

--- easing-ruler-engine.py
+++ easing-ruler-engine.py
@@ -25,6 +25,7 @@ ruler_svg_style = """
 """
 
 import os
+from math import sin, cos, radians
 from textwrap import dedent
 
 # Easing ruler - straight
@@ -37,45 +38,42 @@ for (ruler_easing_function_name, ruler_easing_function) in ruler_easing_function
         exist_ok = True,
     )
 
-    for frames in range(4, 11):
+    for ruler_frames in range(4, 11):
         ruler_svg_path = "rulers/easing-straight-{}/ruler-easing-straight-{}-{}f.svg".format(
             ruler_easing_function_name,
             ruler_easing_function_name,
-            frames,
+            ruler_frames,
         )
 
-        ruler_primary_lines = ""
-        for t in map(lambda line: line / (frames - 1), range(1, frames - 1)):
-            ruler_primary_lines += '<line class="primary" x1="{x}" y1="0" x2="288" y2="384" />'.format(
+        ruler_svg_contents = ""
+        for t in map(lambda line: line / (ruler_frames - 1), range(1, ruler_frames - 1)):
+            ruler_svg_contents += '<line class="primary" x1="{x}" y1="0" x2="288" y2="384" />'.format(
                 x = ruler_easing_function(t) * 576,
             )
 
-        ruler_secondary_lines = ""
         for t in map(lambda line: line / 12, range(1, 12)):
-            ruler_secondary_lines += '<line class="secondary" x1="{x1}" y1="{y}" x2="{x2}" y2="{y}" />'.format(
+            ruler_svg_contents += '<line class="secondary" x1="{x1}" y1="{y}" x2="{x2}" y2="{y}" />'.format(
                 x1 = t * 288,
                 x2 = 576 - (t * 288),
                 y = t * 384,
             )
-        ruler_secondary_lines += '<line class="secondary" x1="288" y1="0" x2="288" y2="384" />'
+        ruler_svg_contents += '<line class="secondary" x1="288" y1="0" x2="288" y2="384" />'
 
-        ruler_svg_data = dedent("""\
+        ruler_svg_document = dedent("""\
             <svg width="576" height="384" viewBox="0 0 576 384" xmlns="http://www.w3.org/2000/svg">
                 {}
                 <g id="ruler">
                     <polygon class="primary" points="0,0 576,0 288,384" />
                     {}
-                    {}
                 </g>
             </svg>
         """).format(
             ruler_svg_style,
-            ruler_primary_lines,
-            ruler_secondary_lines
+            ruler_svg_contents,
         )
 
         with open(ruler_svg_path, "w") as ruler_svg_file:
-            ruler_svg_file.write(ruler_svg_data)
+            ruler_svg_file.write(ruler_svg_document)
 
 # Easing ruler - radial
 
@@ -87,13 +85,66 @@ for (ruler_easing_function_name, ruler_easing_function) in ruler_easing_function
         exist_ok = True,
     )
 
-    for frames in range(4, 11):
-        for degrees in [90, 120, 180, 240, 270, 360]:
+    for ruler_frames in range(4, 11):
+        for ruler_degrees in [90, 120, 180, 240, 270, 360]:
             ruler_svg_path = "rulers/easing-radial-{}/ruler-easing-radial-{}-{}deg-{}f.svg".format(
                 ruler_easing_function_name,
                 ruler_easing_function_name,
-                degrees,
-                frames,
+                ruler_degrees,
+                ruler_frames,
             )
-            # TODO
-            print(ruler_svg_path)
+
+            ruler_inner_radius = 22.5
+            ruler_outer_radius = 288
+
+            ruler_svg_contents = ""
+
+            def circle_arc_point(arc_degrees, arc_radius):
+                return (
+                    288 + cos(radians(arc_degrees)) * arc_radius,
+                    288 - sin(radians(arc_degrees)) * arc_radius,
+                )
+
+            def draw_circle_arc(style_class, arc_radius):
+                arc_resolution = 9
+
+                x, y = circle_arc_point(0, arc_radius)
+                arc_path_data = "M{} {}".format(x, y)
+
+                for l in range(0, arc_resolution):
+                    x, y = circle_arc_point(l / (arc_resolution - 1) * ruler_degrees, arc_radius)
+                    arc_path_data += " A {} {} 0 0 0 {} {}".format(arc_radius, arc_radius, x, y)
+
+                return '<path class="{}" d="{}" />'.format(style_class, arc_path_data)
+
+            def draw_radius(style_class, arc_degrees):
+                x1, y1 = circle_arc_point(arc_degrees, ruler_inner_radius)
+                x2, y2 = circle_arc_point(arc_degrees, ruler_outer_radius)
+                return '<line class="{}" x1="{}" y1="{}" x2="{}" y2="{}" />'.format(style_class, x1, y1, x2, y2)
+
+            ruler_svg_contents += draw_circle_arc("primary", ruler_outer_radius)
+            for l in range(1, 6):
+                ruler_svg_contents += draw_circle_arc("secondary", (l / 6) * ruler_outer_radius)
+
+            for l in range(0, ruler_frames):
+                ruler_svg_contents += draw_radius("primary", ruler_easing_function(l / (ruler_frames - 1)) * ruler_degrees)
+
+            ruler_svg_document = dedent("""\
+                <svg width="576" height="576" viewBox="0 0 576 576" xmlns="http://www.w3.org/2000/svg">
+                    {}
+                    <g id="ruler">
+                        <circle class="secondary" cx="288" cy="288" r="288" />
+                        <circle class="primary" cx="288" cy="288" r="9" />
+                        <circle class="primary" cx="288" cy="288" r="22.5" />
+                        <line class="primary" x1="270" y1="288" x2="306" y2="288" />
+                        <line class="primary" x1="288" y1="270" x2="288" y2="306" />
+                        {}
+                    </g>
+                </svg>
+            """).format(
+                ruler_svg_style,
+                ruler_svg_contents,
+            )
+
+            with open(ruler_svg_path, "w") as ruler_svg_file:
+                ruler_svg_file.write(ruler_svg_document)
