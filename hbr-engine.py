90b93c27df2c1d6edc5228b99bb855604e7c77fb
Nagy Tibor <xnagytibor@protonmail.com>
2023-09-17 19:27:01 +0200

    Add simple hbr style

--- hbr-engine.py
+++ hbr-engine.py
@@ -25,6 +25,11 @@ head_body_ratios = {
     "1:6-standard":     [1.000, 0.900, 0.800, 1.000, 0.500, 0.000, 0.100,     1,     1,     3],
 }
 
+guide_styles = [
+    "figure",
+    "simple",
+]
+
 def draw_lines(lines):
     svg_data = ""
     for ([(x1, y1), (x2, y2)], mirror) in lines:
@@ -64,7 +69,7 @@ def draw_lines(lines):
 #    |     |  o  o  o  |     |       |  |
 #    +-----|---ooooo---|-----+ 384   -  -
 
-def create_hbr_guide(body_ratios):
+def create_hbr_guide(body_ratios, guide_style):
     head_width_ratio,  shoulder_width_ratio,    waist_width_ratio,       \
     hip_width_ratio,   feet_width_ratio,        feet_separation_ratio,   \
     neck_length_ratio, upper_body_length_ratio, lower_body_length_ratio, \
@@ -81,22 +86,30 @@ def create_hbr_guide(body_ratios):
     head_width, head_height = head_width_ratio * svg_content_width, svg_content_width
     eyes_width, eyes_height = 0.5, 0.25
 
-    svg_contents += '<ellipse class="secondary" cx="{cx}" cy="{cy}" rx="{rx}" ry="{ry}" />'.format(
-        cx = svg_format_float(head_width  * 0.0),
-        cy = svg_format_float(head_height * 0.5),
-        rx = svg_format_float(head_width  * 0.5),
-        ry = svg_format_float(head_height * 0.5),
-    )
+    match guide_style:
+        case "figure":
+            svg_contents += '<ellipse class="secondary" cx="{cx}" cy="{cy}" rx="{rx}" ry="{ry}" />'.format(
+                cx = svg_format_float(head_width  * 0.0),
+                cy = svg_format_float(head_height * 0.5),
+                rx = svg_format_float(head_width  * 0.5),
+                ry = svg_format_float(head_height * 0.5),
+            )
 
-    svg_contents += draw_lines([
-        ([(head_width * -0.5, head_height * 0.5), (head_width * 0.5, head_height * 0.5)], False),
-        ([(head_width *  0.0, head_height * 0.0), (head_width * 0.0, head_height * 1.0)], False),
-    ])
+            svg_contents += draw_lines([
+                ([(head_width * -0.5, head_height * 0.5), (head_width * 0.5, head_height * 0.5)], False),
+                ([(head_width *  0.0, head_height * 0.0), (head_width * 0.0, head_height * 1.0)], False),
+            ])
 
-    svg_contents += draw_lines([
-        ([(head_width * (eyes_width * 0.5), head_height * (0.5 - eyes_height * 0.5)),
-          (head_width * (eyes_width * 0.5), head_height * (0.5 + eyes_height * 0.5))], True),
-    ])
+            svg_contents += draw_lines([
+                ([(head_width * (eyes_width * 0.5), head_height * (0.5 - eyes_height * 0.5)),
+                  (head_width * (eyes_width * 0.5), head_height * (0.5 + eyes_height * 0.5))], True),
+            ])
+        case "simple":
+            svg_contents += '<circle class="secondary" cx="{cx}" cy="{cy}" r="{r}" />'.format(
+                cx = svg_format_float(svg_content_width * 0.0),
+                cy = svg_format_float(svg_content_width * 0.5),
+                r  = svg_format_float(svg_content_width * 0.5),
+            )
 
     # Body
     x_shoulder = (shoulder_width_ratio  * 0.5) * svg_content_width
@@ -111,16 +124,25 @@ def create_hbr_guide(body_ratios):
     y_hip      = (1 + neck_length_ratio + upper_body_length_ratio + lower_body_length_ratio                    ) * head_height
     y_feet     = (1 + neck_length_ratio + upper_body_length_ratio + lower_body_length_ratio + legs_length_ratio) * head_height
 
-    svg_contents += draw_lines([
-        ([(-x_shoulder, y_shoulder), (x_shoulder, y_shoulder)], False),
-        ([(-x_waist,    y_waist   ), (x_waist,    y_waist   )], False),
-        ([(-x_hip,      y_hip     ), (x_hip,      y_hip     )], False),
-        ([( x_shoulder, y_shoulder), (x_waist,    y_waist   )], True ),
-        ([( x_waist,    y_waist   ), (x_hip,      y_hip     )], True ),
-        ([( x_groin,    y_hip     ), (x1_feet,    y_feet,   )], True ),
-        ([( x_hip,      y_hip     ), (x2_feet,    y_feet,   )], True ),
-        ([( x1_feet,    y_feet    ), (x2_feet,    y_feet    )], True ),
-    ])
+    match guide_style:
+        case "figure":
+            svg_contents += draw_lines([
+                ([(-x_shoulder, y_shoulder), (x_shoulder, y_shoulder)], False),
+                ([(-x_waist,    y_waist   ), (x_waist,    y_waist   )], False),
+                ([(-x_hip,      y_hip     ), (x_hip,      y_hip     )], False),
+                ([( x_shoulder, y_shoulder), (x_waist,    y_waist   )], True ),
+                ([( x_waist,    y_waist   ), (x_hip,      y_hip     )], True ),
+                ([( x_groin,    y_hip     ), (x1_feet,    y_feet,   )], True ),
+                ([( x_hip,      y_hip     ), (x2_feet,    y_feet,   )], True ),
+                ([( x1_feet,    y_feet    ), (x2_feet,    y_feet    )], True ),
+            ])
+        case "simple":
+            svg_contents += draw_lines([
+                ([(-svg_content_width * 0.5, y_shoulder), (svg_content_width * 0.5, y_shoulder)], False),
+                ([(-svg_content_width * 0.5, y_waist   ), (svg_content_width * 0.5, y_waist   )], False),
+                ([(-svg_content_width * 0.5, y_hip     ), (svg_content_width * 0.5, y_hip     )], False),
+                ([(-svg_content_width * 0.5, y_feet    ), (svg_content_width * 0.5, y_feet    )], False),
+            ])
 
     return dedent("""\
         <svg width="{svg_width}" height="{svg_height}" viewBox="{svg_vbox_x} 0 {svg_width} {svg_height}" xmlns="http://www.w3.org/2000/svg">
@@ -137,10 +159,11 @@ def create_hbr_guide(body_ratios):
         svg_contents = svg_contents,
     )
 
-for (hbr_name, body_ratios) in head_body_ratios.items():
-    svg_path = "character/hbr_new/hbr-{}.svg".format(hbr_name)
-    svg_document = create_hbr_guide(body_ratios)
+for guide_style in guide_styles:
+    for (hbr_name, body_ratios) in head_body_ratios.items():
+        svg_path = "character/hbr-{}/hbr-{}.svg".format(guide_style, hbr_name)
+        svg_document = create_hbr_guide(body_ratios, guide_style)
 
-    os.makedirs(os.path.dirname(svg_path), exist_ok = True)
-    with open(svg_path, "w") as svg_file:
-        svg_file.write(svg_document)
+        os.makedirs(os.path.dirname(svg_path), exist_ok = True)
+        with open(svg_path, "w") as svg_file:
+            svg_file.write(svg_document)
