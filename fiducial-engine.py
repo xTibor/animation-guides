36aa7c75aa9036f67a4905b53b0cf5ced8823666
Nagy Tibor <xnagytibor@protonmail.com>
2024-04-22 11:16:14 +0200

    More work on fiducial-engine.py

--- fiducial-engine.py
+++ fiducial-engine.py
@@ -13,66 +13,63 @@ fiducial_styles = [
     "xt16bfm",
 ]
 
+svg_style = """
+    <style>
+    .background {
+        fill: #FFFFFF;
+        stroke: #FFFFFF;
+        stroke-width: 1px;
+        stroke-linejoin: round;
+    }
+
+    .foreground {
+        fill: #000000;
+        stroke: #000000;
+        stroke-width: 1px;
+        stroke-linejoin: round;
+    }
+    </style>
+"""
+
 ################################################################################
 #  SVG document generator
 
 def create_fiducial(fiducial_value, fiducial_style):
     match fiducial_style:
         case "xt16bfm":
-            svg_fiducial_style = """
-                <style>
-                .background {
-                    fill: #FFFFFF;
-                    stroke: #FFFFFF;
-                    stroke-width: 1px;
-                    stroke-linejoin: round;
-                }
-
-                .foreground {
-                    fill: #000000;
-                    stroke: #000000;
-                    stroke-width: 1px;
-                    stroke-linejoin: round;
-                }
-                </style>
-            """
-
             svg_contents = ""
 
-            tile_size = 144
-            tile_positions = [
-                [ 72,  72], [216,  72], [360,  72], [360, 216],
-                [360, 360], [216, 360], [ 72, 360], [ 72, 216],
-            ]
-
             for tile_position_index in range(0, 8):
-                [tile_x, tile_y] = tile_positions[tile_position_index]
+                tile_size = 144
+
+                [tile_u, tile_v] = [
+                    [0, 0], [1, 0], [2, 0], [2, 1],
+                    [2, 2], [1, 2], [0, 2], [0, 1],
+                ][tile_position_index]
+
+                tile_x0 = 72 + (tile_u * tile_size)
+                tile_y0 = 72 + (tile_v * tile_size)
+                tile_x1 = tile_x0 + tile_size
+                tile_y1 = tile_y0 + tile_size
+
                 tile_index = (fiducial_value >> (tile_position_index * 2)) & 0b11
 
                 match tile_index:
                     case 0:
                         svg_contents += '<polygon class="foreground" points="{},{} {},{} {},{}" />'.format(
-                            tile_x,             tile_y,
-                            tile_x + tile_size, tile_y,
-                            tile_x,             tile_y + tile_size,
+                            tile_x0, tile_y0, tile_x1, tile_y0, tile_x0, tile_y1,
                         )
                     case 1:
                         svg_contents += '<polygon class="foreground" points="{},{} {},{} {},{}" />'.format(
-                            tile_x,             tile_y,
-                            tile_x + tile_size, tile_y,
-                            tile_x + tile_size, tile_y + tile_size,
+                            tile_x0, tile_y0, tile_x1, tile_y0, tile_x1, tile_y1,
                         )
                     case 2:
                         svg_contents += '<polygon class="foreground" points="{},{} {},{} {},{}" />'.format(
-                            tile_x + tile_size, tile_y,
-                            tile_x + tile_size, tile_y + tile_size,
-                            tile_x,             tile_y + tile_size,
+                            tile_x1, tile_y0, tile_x1, tile_y1, tile_x0, tile_y1,
                         )
                     case 3:
                         svg_contents += '<polygon class="foreground" points="{},{} {},{} {},{}" />'.format(
-                            tile_x,             tile_y,
-                            tile_x + tile_size, tile_y + tile_size,
-                            tile_x,             tile_y + tile_size ,
+                            tile_x0, tile_y0, tile_x1, tile_y1, tile_x0, tile_y1,
                         )
 
             return dedent("""\
@@ -89,7 +86,7 @@ def create_fiducial(fiducial_value, fiducial_style):
                     </g>
                 </svg>
             """).format(
-                svg_fiducial_style,
+                svg_style,
                 fiducial_style,
                 fiducial_value,
                 svg_contents,
