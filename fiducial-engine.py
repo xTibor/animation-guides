d83f7189aa9288bb015197a4323dabe9d91f75a1
Nagy Tibor <xnagytibor@protonmail.com>
2024-04-22 13:31:30 +0200

    Only accept fiducial values in canonical form

--- fiducial-engine.py
+++ fiducial-engine.py
@@ -32,36 +32,69 @@ svg_style = """
 """
 
 ################################################################################
+#  Fiducial - xt16bfm
+
+def xt16bfm_rotate(value):
+    result = 0
+    for [index_a, index_b] in enumerate([6, 7, 0, 1, 2, 3, 4, 5]):
+        tmp = (value >> (index_b * 2)) & 0b11
+        tmp = (tmp + 1) & 0b11
+        result |= (tmp << (index_a * 2))
+    return result
+
+def xt16bfm_mirror(value):
+    result = 0
+    for [index_a, index_b] in enumerate([2, 1, 0, 7, 6, 5, 4, 3]):
+        tmp = (value >> (index_b * 2)) & 0b11
+        tmp = tmp ^ 0b01
+        result |= (tmp << (index_a * 2))
+    return result
+
+def xt16bfm_canonicalize(value):
+    return min([
+        value,
+        xt16bfm_rotate(value),
+        xt16bfm_rotate(xt16bfm_rotate(value)),
+        xt16bfm_rotate(xt16bfm_rotate(xt16bfm_rotate(value))),
+        xt16bfm_mirror(value),
+        xt16bfm_rotate(xt16bfm_mirror(value)),
+        xt16bfm_rotate(xt16bfm_rotate(xt16bfm_mirror(value))),
+        xt16bfm_rotate(xt16bfm_rotate(xt16bfm_rotate(xt16bfm_mirror(value)))),
+    ])
+
+################################################################################
 #  SVG document generator
 
 def create_fiducial(fiducial_value, fiducial_style):
     match fiducial_style:
         case "xt16bfm":
             svg_contents = ""
-            tile_size = 144
 
-            for tile_position_index in range(0, 8):
-                tile_index = (fiducial_value >> (tile_position_index * 2)) & 0b11
+            if fiducial_value == xt16bfm_canonicalize(fiducial_value):
+                for tile_position_index in range(0, 8):
+                    tile_index = (fiducial_value >> (tile_position_index * 2)) & 0b11
 
-                [tile_u, tile_v] = [
-                    [0, 0], [1, 0], [2, 0], [2, 1],
-                    [2, 2], [1, 2], [0, 2], [0, 1],
-                ][tile_position_index]
+                    [tile_u, tile_v] = [
+                        [0, 0], [1, 0], [2, 0], [2, 1],
+                        [2, 2], [1, 2], [0, 2], [0, 1],
+                    ][tile_position_index]
 
-                tile_x0 = 72 + (tile_u * tile_size)
-                tile_y0 = 72 + (tile_v * tile_size)
+                    tile_size = 144
 
-                tile_x1 = tile_x0 + tile_size
-                tile_y1 = tile_y0 + tile_size
+                    tile_x0 = 72 + (tile_u * tile_size)
+                    tile_y0 = 72 + (tile_v * tile_size)
 
-                svg_contents += '<polygon class="foreground" points="{},{} {},{} {},{}" />'.format(
-                    *[
-                        [tile_x0, tile_y0, tile_x1, tile_y0, tile_x0, tile_y1],
-                        [tile_x0, tile_y0, tile_x1, tile_y0, tile_x1, tile_y1],
-                        [tile_x1, tile_y0, tile_x1, tile_y1, tile_x0, tile_y1],
-                        [tile_x0, tile_y0, tile_x1, tile_y1, tile_x0, tile_y1],
-                    ][tile_index]
-                )
+                    tile_x1 = tile_x0 + tile_size
+                    tile_y1 = tile_y0 + tile_size
+
+                    svg_contents += '<polygon class="foreground" points="{},{} {},{} {},{}" />'.format(
+                        *[
+                            [tile_x0, tile_y0, tile_x1, tile_y0, tile_x0, tile_y1],
+                            [tile_x0, tile_y0, tile_x1, tile_y0, tile_x1, tile_y1],
+                            [tile_x1, tile_y0, tile_x1, tile_y1, tile_x0, tile_y1],
+                            [tile_x0, tile_y0, tile_x1, tile_y1, tile_x0, tile_y1],
+                        ][tile_index]
+                    )
 
             return dedent("""\
                 <svg width="576" height="576" viewBox="0 0 576 576" xmlns="http://www.w3.org/2000/svg">
